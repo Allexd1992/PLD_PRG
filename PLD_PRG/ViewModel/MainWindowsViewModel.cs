bd8bafbfe561ea699a43ebff846170769259303a
Aleksandr Abakumov <allexd@live.ru>
2020-01-14 17:26:50 +0300

    Добавьте файлы проекта.

---
+++ PLD_PRG/ViewModel/MainWindowsViewModel.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+using Prism.Commands;
+using System.Windows.Threading;
+using GalaSoft.MvvmLight;
+using DocumentFormat.OpenXml.Bibliography;
+using System.Windows.Controls;
+
+namespace PLD_PRG.ViewModel 
+{
+    class MainWindowsViewModel :ViewModelBase, INotifyPropertyChanged
+    {
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        // TODO 01:Tag
+        #region Tag
+        private Page PageHome;
+        private Page PageReel;
+        private Page PageVacuum;
+        private Page PageTarget;
+        private Page PageLaser;
+        private Page PageScan;
+        private Page PageHeater;
+        private Page PageEventLog;
+        private Page PageLogs;
+        private Page PageDiagnostic;
+        private Page PageSetting;
+        private Page _currentPage;
+        public Page CurrentPage
+        {
+            get { return _currentPage; }
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged("CurrentPage");
+                GC.Collect();
+            }
+        }
+        private int _frameOpacity;
+        public int FrameOpacity
+        {
+            get { return _frameOpacity; }
+            set
+            {
+                _frameOpacity = value;
+                OnPropertyChanged("FrameOpacity");
+                GC.Collect();
+            }
+        }
+        private string psTime;
+        public string sTime
+        {
+            get { return psTime; }
+            set
+            {
+                psTime = value;
+                OnPropertyChanged("sTime");
+                GC.Collect();
+            }
+        }
+
+        private string pdTime;
+        public string dTime
+        {
+            get { return pdTime; }
+            set
+            {
+                pdTime = value;
+                OnPropertyChanged("dTime");
+                GC.Collect();
+            }
+        }
+        #endregion
+        // TODO 02 :Button Menu
+        #region ButtonMenu
+
+        public ICommand IHomePage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageHome;
+                    GC.Collect();
+                });
+                
+            }
+
+        }
+        public ICommand IReelPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageReel;
+                    GC.Collect();
+                });
+                
+            }
+        }
+        public ICommand IVacuumPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageVacuum;
+                    GC.Collect();
+                });
+
+            }
+        }
+        public ICommand ITargetPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageTarget;
+                    GC.Collect();
+                });
+
+            }
+        }
+        public ICommand IScanPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageScan;
+                    GC.Collect();
+                });
+
+            }
+        }
+        public ICommand IHeaterPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageHeater;
+                    GC.Collect();
+                });
+
+            }
+        }
+
+        public ICommand IEventLogPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageEventLog;
+                    GC.Collect();
+                });
+
+            }
+        }
+        public ICommand ILogsPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageLogs;
+                    GC.Collect();
+                });
+
+            }
+        }
+        public ICommand ILaserPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageLaser;
+                    GC.Collect();
+                });
+
+            }
+        }
+        public ICommand IDiagnosticPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageDiagnostic;
+                    GC.Collect();
+                });
+
+            }
+        }
+        public ICommand ISettingPage
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    CurrentPage = PageSetting;
+                    GC.Collect();
+                });
+
+            }
+        }
+        #endregion
+        public MainWindowsViewModel()
+        {
+            PageHome = new View.PageHome();
+            PageReel = new View.PageReel();
+            PageVacuum = new View.PageVacuum();
+            PageTarget = new View.PageTarget();
+            PageLaser = new View.PageLaser();
+            PageScan = new View.PageScan();
+            PageHeater = new View.PageHeater();
+            PageEventLog = new View.PageEventLog();
+            PageLogs = new View.PageLogs();
+            PageDiagnostic = new View.PageDiagnostic();
+            PageSetting = new View.PageSetting();
+            CurrentPage = PageHome;
+            FrameOpacity = 1;
+            var timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            sTime = Convert.ToString((DateTime.Now)).Split(' ')[1];
+            dTime = Convert.ToString((DateTime.Now)).Split(' ')[0];
+        }
+    }
+}
---
+++ PLD_PRG/ViewModel/PageLaserViewModel.cs
@@ -0,0 +1,287 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace PLD_PRG.ViewModel
+{
+    class PageLaserViewModel : INotifyPropertyChanged
+    {
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Tag
+        private string psTime;
+        public string sTime
+        {
+            get { return psTime; }
+            set
+            {
+                psTime = value;
+                OnPropertyChanged("sTime");
+                GC.Collect();
+            }
+        }
+
+        private string pdTime;
+        public string dTime
+        {
+            get { return pdTime; }
+            set
+            {
+                pdTime = value;
+                OnPropertyChanged("dTime");
+                GC.Collect();
+            }
+        }
+
+        private bool pBlMb;
+        public bool BlMb
+        {
+            get { return pBlMb; }
+            set
+            {
+                pBlMb = value;
+                OnPropertyChanged("BlMb");
+                GC.Collect();
+            }
+        }
+
+        private bool pBlCom;
+        public bool BlCom
+        {
+            get { return pBlCom; }
+            set
+            {
+                pBlCom = value;
+                OnPropertyChanged("BlCom");
+                GC.Collect();
+            }
+        }
+
+        private string pPressure;
+        public string pressure
+        {
+            get { return pPressure; }
+            set
+            {
+                pPressure = value;
+                OnPropertyChanged("pressure");
+                GC.Collect();
+            }
+
+        }
+
+        private string pReprate;
+        public string reprate
+        {
+            get { return pReprate; }
+            set
+            {
+                pReprate = value;
+                OnPropertyChanged("reprate");
+                GC.Collect();
+            }
+
+        }
+
+        private string pShutter;
+        public string shutter
+        {
+            get { return pShutter; }
+            set
+            {
+                pShutter = value;
+                OnPropertyChanged("shutter");
+                GC.Collect();
+            }
+
+        }
+
+        private string pEgy;
+        public string egy
+        {
+            get { return pEgy; }
+            set
+            {
+                pEgy = value;
+                OnPropertyChanged("egy");
+                GC.Collect();
+            }
+
+        }
+
+        private string pHv;
+        public string hv
+        {
+            get { return pHv; }
+            set
+            {
+                pHv = value;
+                OnPropertyChanged("hv");
+                GC.Collect();
+            }
+
+        }
+
+        private string pCounter;
+        public string counter
+        {
+            get { return pCounter; }
+            set
+            {
+                pCounter = value;
+                OnPropertyChanged("counter");
+                GC.Collect();
+            }
+
+        }
+
+        private string pCounterNewFill;
+        public string counterNewFill
+        {
+            get { return pCounterNewFill; }
+            set
+            {
+                pCounterNewFill = value;
+                OnPropertyChanged("counterNewFill");
+                GC.Collect();
+            }
+
+        }
+
+        private string pTrigger;
+        public string trigger
+        {
+            get { return pTrigger; }
+            set
+            {
+                pTrigger = value;
+                OnPropertyChanged("trigger");
+                GC.Collect();
+            }
+
+        }
+
+        private string pTubeTemp;
+        public string tubeTemp
+        {
+            get { return pTubeTemp; }
+            set
+            {
+                pTubeTemp = value;
+                OnPropertyChanged("tubeTemp");
+                GC.Collect();
+            }
+
+        }
+
+        private string pMode;
+        public string mode
+        {
+            get { return pMode; }
+            set
+            {
+                pMode = value;
+                OnPropertyChanged("mode");
+                GC.Collect();
+            }
+
+        }
+
+        private string pOpMode;
+        public string opMode
+        {
+            get { return pOpMode; }
+            set
+            {
+                pOpMode = value;
+                OnPropertyChanged("opMode");
+                GC.Collect();
+            }
+
+        }
+
+        private string pTechTime;
+        public string techTime
+        {
+            get { return pTechTime; }
+            set
+            {
+                pTechTime = value;
+                OnPropertyChanged("techTime");
+                GC.Collect();
+            }
+
+        }
+
+        private string pStCod;
+        public string stCod
+        {
+            get { return pStCod; }
+            set
+            {
+                pStCod = value;
+                OnPropertyChanged("stCod");
+                GC.Collect();
+            }
+
+        }
+
+        #endregion
+        public ICommand BtnClkMbRun
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    BlMb = true;
+    
+
+                });
+            }
+        }
+        public ICommand BtnClkMbStop
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    BlMb = false;
+               
+                });
+            }
+        }
+        public ICommand BtnClkComRun
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    BlCom = true;
+                    // MainModel.LserComConnect();
+                   
+
+                });
+            }
+        }
+        public ICommand BtnClkComStop
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+
+                    //MainModel.LserComDisconnect();
+                    BlCom = false;
+                });
+            }
+        }
+
+    }
+}
